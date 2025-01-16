55eafda43349cc3f964b752774af7c7e75eac72c
Pavel Rotov <paroto@taltech.ee>
2025-01-16 16:35:42 +0200

    src/wsfs.c: sockets are opened now

--- src/wsfs.c
+++ src/wsfs.c
@@ -3,10 +3,10 @@
 #include <arpa/inet.h>
 #include <config.h>
 #include <ctype.h>
-#include <stdint.h>
 #include <getopt.h>
 #include <limits.h>
 #include <netinet/in.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,11 +18,31 @@
 
 #define DEF_PORT 8080
 #define OPTION_ERROR 1
+#define SERVER_BACKLOG 1
 
 #define S_EQ(a, b) (strcmp (a, b) == 0)
 
+enum LONG_OPTS
+{
+  OPT_LOG_LEVEL = 1,
+  OPT_LOG_OUTPUT,
+  OPT_INET4_SOURCE,
+  OPT_INET6_SOURCE,
+  OPT_INET4_PORT,
+  OPT_INET6_PORT,
+  OPT_TARGET,
+};
+
+enum IP_MODE
+{
+  IPV4 = 1,
+  IPV6 = 2,
+};
+
 int in4_socket (struct in_addr *sin4_addr, in_port_t sin4_port);
 int in6_socket (struct in6_addr *sin6_addr, in_port_t sin6_port);
+
+int handle_connection (int client_socketfd);
 
 // Printf info for users
 void printf_help ();
@@ -38,6 +58,10 @@ int handle_target (char *out, char *optarg);
 
 int check (int exp, const char *msg);
 
+// General
+static int pid6 = -1;
+static uint8_t mode = IPV6 | IPV4;
+
 // Info options flags
 static int verbose_flag;
 static int help_flag;
@@ -49,32 +73,26 @@ static char log_output[PATH_MAX];
 
 // IPv4 options
 static int sin4_only_flag = 0;
-static struct in_addr sin4_addr;
-static in_port_t sin4_port;
+static struct in_addr sin4_addr = { .s_addr = INADDR_ANY };
+static in_port_t sin4_port = DEF_PORT;
 
 // IPv6 options
 static int sin6_only_flag = 0;
 static struct in6_addr sin6_addr;
-static in_port_t sin6_port;
+static in_port_t sin6_port = DEF_PORT;
 
 // Target options
 static char target[PATH_MAX];
-
-enum LONG_OPTS
-{
-  OPT_LOG_LEVEL = 1,
-  OPT_LOG_OUTPUT,
-  OPT_INET4_SOURCE,
-  OPT_INET6_SOURCE,
-  OPT_INET4_PORT,
-  OPT_INET6_PORT,
-  OPT_TARGET,
-};
 
 int
 main (int argc, char *argv[])
 {
   int c;
+  int in4_socketfd, in6_socketfd, in_socketfd;
+  memcpy (&sin6_addr, &in6addr_any, sizeof (in6addr_any));
+
+  int client_socketfd;
+  size_t client_addr_size;
 
   while (1)
     {
@@ -121,22 +139,27 @@ main (int argc, char *argv[])
                  "available options.\n");
           break;
         case OPT_LOG_OUTPUT:
-          check (handle_log_output (log_output, optarg), "\n");
+          check (handle_log_output (log_output, optarg),
+                 "wsfs: --log-output fail.\n");
           break;
         case OPT_INET4_SOURCE:
-          check (handle_sin4_addr (&sin4_addr, optarg), "\n");
+          check (handle_sin4_addr (&sin4_addr, optarg),
+                 "wsfs: --source4 fail.\n");
           break;
         case OPT_INET6_SOURCE:
-          check (handle_sin6_addr (&sin6_addr, optarg), "\n");
+          check (handle_sin6_addr (&sin6_addr, optarg),
+                 "wsfs: --source6 fail.\n");
           break;
         case OPT_INET4_PORT:
-          check (handle_sin_port (&sin4_port, optarg), "\n");
+          check (handle_sin_port (&sin4_port, optarg),
+                 "wsfs: --port4 fail.\n");
           break;
         case OPT_INET6_PORT:
-          check (handle_sin_port (&sin6_port, optarg), "\n");
+          check (handle_sin_port (&sin6_port, optarg),
+                 "wsfs: --port6 fail.\n");
           break;
         case OPT_TARGET:
-          check (handle_target (target, optarg), "\n");
+          check (handle_target (target, optarg), "wsfs: --target fail.\n");
           break;
 
         case '?':
@@ -161,19 +184,106 @@ main (int argc, char *argv[])
       exit (EXIT_SUCCESS);
     }
 
-  check(sin4_only_flag && sin6_only_flag, "--only4 argument conflicts with --only6 argument\n");
+  check (sin4_only_flag && sin6_only_flag,
+         "--only4 argument conflicts with --only6 argument\n");
+
+  if (sin4_only_flag)
+    mode ^= IPV6;
+  if (sin6_only_flag)
+    mode ^= IPV4;
+
+  if (mode == (IPV6 | IPV4))
+    {
+      // you may ask "Why fork()?"
+      // Why not? <https://youtu.be/kVTx0JFQCkg?si=DJzJolab_20zk0I5&t=30>
+      if ((pid6 = fork ()) == 0)
+        in_socketfd = in6_socketfd = in6_socket (&sin6_addr, sin6_port);
+      else
+        in_socketfd = in4_socketfd = in4_socket (&sin4_addr, sin4_port);
+    }
+  else if (mode == IPV6)
+    in_socketfd = in6_socketfd = in6_socket (&sin6_addr, sin6_port);
+  else
+    in_socketfd = in4_socketfd = in4_socket (&sin4_addr, sin4_port);
+
+  struct sockaddr_in client_addr;
+  while (1)
+    {
+      client_addr_size = sizeof (struct sockaddr_in);
+      check ((client_socketfd
+              = accept (in_socketfd, (struct sockaddr *)&client_addr,
+                        (socklen_t *)&client_addr_size))
+                 == -1,
+             "accept failed.\n");
+      handle_connection (client_socketfd);
+    }
 
   exit (EXIT_SUCCESS);
 }
 
 int
+handle_connection (int client_socketfd)
+{
+  return 0;
+}
+
+int
 in4_socket (struct in_addr *sin4_addr, in_port_t sin4_port)
 {
+  int opt = 1;
+  int in4_socketfd;
+
+  check ((in4_socketfd = socket (AF_INET, SOCK_STREAM, 0)) == -1,
+         "ipv4: socket creation failed.\n");
+
+  check (setsockopt (in4_socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                     &opt, sizeof (opt)),
+         "ipv4: socket option setting failed.\n");
+
+  struct sockaddr_in in4_sockaddr;
+  in4_sockaddr.sin_family = AF_INET;
+  in4_sockaddr.sin_port = htons (sin4_port);
+  memcpy (&in4_sockaddr.sin_addr, sin4_addr, sizeof (sin4_addr));
+
+  check (bind (in4_socketfd, (struct sockaddr *)&in4_sockaddr,
+               sizeof (in4_sockaddr)),
+         "ipv4: bind failed.\n");
+
+  check (listen (in4_socketfd, SERVER_BACKLOG), "ipv4: listen failed.\n");
+
+  return in4_socketfd;
 }
 
 int
 in6_socket (struct in6_addr *sin6_addr, in_port_t sin6_port)
 {
+  int opt = 1;
+  int in6_socketfd;
+
+  check ((in6_socketfd = socket (AF_INET6, SOCK_STREAM, 0)) == -1,
+         "ipv6: socket creation failed.\n");
+
+  check (setsockopt (in6_socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                     &opt, sizeof (opt)),
+         "ipv6: socket option setting failed.\n");
+
+  check (
+      setsockopt (in6_socketfd, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof (opt)),
+      "ipv6: socket protocol level option setting failed.\n");
+
+  struct sockaddr_in6 in6_sockaddr;
+  in6_sockaddr.sin6_family = AF_INET6;
+  in6_sockaddr.sin6_port = htons (sin6_port);
+  in6_sockaddr.sin6_addr = in6addr_any;
+  // memcpy (&in6_sockaddr.sin6_addr, sin6_addr, sizeof (sin6_addr));
+
+  check (bind (in6_socketfd, (struct sockaddr *)&in6_sockaddr,
+               sizeof (in6_sockaddr)),
+         "ipv6: bind failed.\n");
+
+  check (listen (in6_socketfd, SERVER_BACKLOG), "ipv6: listen failed.\n");
+
+  return in6_socketfd;
 }
 
 int
@@ -181,6 +291,7 @@ check (int exp, const char *msg)
 {
   if (exp != 0)
     {
+      perror ("wsfs");
       // TODO: replace with logging macro/function
       fprintf (stderr, "%s", msg);
       exit (EXIT_FAILURE);
@@ -257,7 +368,7 @@ handle_log_output (char *out, char *optarg)
 int
 handle_sin4_addr (struct in_addr *out, char *optarg)
 {
-  if (inet_aton (optarg, out) != 0)
+  if (inet_aton (optarg, out) == 0)
     return OPTION_ERROR;
   return 0;
 }
@@ -265,11 +376,11 @@ handle_sin4_addr (struct in_addr *out, char *optarg)
 int
 handle_sin_port (in_port_t *out, char *optarg)
 {
-  int temp = atoi(optarg);
-  if (temp <= 0 || temp > UINT16_MAX) 
+  int temp = atoi (optarg);
+  if (temp <= 0 || temp > UINT16_MAX)
     return OPTION_ERROR;
 
-  *out = (uint16_t) temp;
+  *out = (uint16_t)temp;
   return 0;
 }
 
@@ -277,18 +388,20 @@ int
 handle_sin6_addr (struct in6_addr *out, char *optarg)
 {
   int status;
-  if ( (status = inet_pton (AF_INET6, optarg, out)) == 1)
+  if ((status = inet_pton (AF_INET6, optarg, out)) == 1)
     return 0;
   else if (status == 0)
-  {
-    fprintf(stderr, "source6: does not contain a character string representing a valid network address.\n");
-    return OPTION_ERROR;
-  }
+    {
+      fprintf (stderr,
+               "source6: does not contain a character string representing a "
+               "valid network address.\n");
+      return OPTION_ERROR;
+    }
   else
-  {
-    perror("source6");
-    return OPTION_ERROR;
-  }
+    {
+      perror ("source6");
+      return OPTION_ERROR;
+    }
 }
 
 int
