b092f82778dae62f5e55f4b077b40290d67ff377
Pavel Rotov <paroto@taltech.ee>
2025-01-20 14:07:20 +0200

    src/http_utils.h: init

---
+++ src/http_utils.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+#ifndef _HTTP_FUNC
+#define _HTTP_FUNC
+
+int http_parse_request (struct http_request *out, int socketfd);
+int http_construct_response (struct http_response *out, struct http_request *request);
+int http_cache_set (struct http_response *response);
+int http_cache_get (struct http_response *response);
+
+#endif
