5bd6b7f45f69a531c8d956dca1077d665d2cb72c
Pavel Rotov <paroto@taltech.ee>
2025-01-22 15:47:43 +0200

    src/http_utils.h: added http_status_string_get()

--- src/http_utils.h
+++ src/http_utils.h
@@ -7,5 +7,6 @@ int http_parse_request (struct http_request *out, int socketfd);
 int http_construct_response (struct http_response *out, struct http_request *request);
 int http_cache_set (struct http_response *response);
 int http_cache_get (struct http_response *response);
+int http_status_string_get (char* out, int status);
 
 #endif
