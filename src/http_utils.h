bb779c9ef7fef67ffb0a9591e1551a85376be739
Pavel Rotov <paroto@taltech.ee>
2025-01-22 16:15:50 +0200

    new clang-format applied

--- src/http_utils.h
+++ src/http_utils.h
@@ -3,10 +3,10 @@
 #ifndef _HTTP_FUNC
 #define _HTTP_FUNC
 
-int http_parse_request (struct http_request *out, int socketfd);
-int http_construct_response (struct http_response *out, struct http_request *request);
-int http_cache_set (struct http_response *response);
-int http_cache_get (struct http_response *response);
-int http_status_string_get (char* out, int status);
+int http_parse_request(struct http_request *out, int socketfd);
+int http_construct_response(struct http_response *out, struct http_request *request);
+int http_cache_set(struct http_response *response);
+int http_cache_get(struct http_response *response);
+int http_status_string_get(char *out, int status);
 
 #endif
