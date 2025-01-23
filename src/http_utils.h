09a0b8886b4448d1c494faa4e72821e03e777980
Pavel Rotov <paroto@taltech.ee>
2025-01-23 20:34:41 +0200

    src/http_utils.h: fixed http request and response types

--- src/http_utils.h
+++ src/http_utils.h
@@ -3,10 +3,10 @@
 #ifndef _HTTP_FUNC
 #define _HTTP_FUNC
 
-int http_parse_request(struct http_request *out, int socketfd);
-int http_construct_response(struct http_response *out, struct http_request *request);
-int http_cache_set(struct http_response *response);
-int http_cache_get(struct http_response *response);
+int http_parse_request(http_request_t *out, int socketfd);
+int http_construct_response(http_response_t *out, http_request_t *request);
+int http_cache_set(http_response_t *response);
+int http_cache_get(http_response_t *response);
 int http_status_string_get(char *out, int status);
 
 #endif
