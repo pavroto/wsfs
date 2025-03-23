025568bcb000b87a498eff9b48534bc3a06f8629
Pavel Rotov <paroto@taltech.ee>
2025-03-23 13:37:11 +0200

    logger: log format changed

--- src/logger.c
+++ src/logger.c
@@ -1,19 +1,87 @@
 #include <linux/limits.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "log_levels.h"
+#include "wsfs_core.h"
 
 int log_level = LOGL_WARN;
 char log_output[PATH_MAX];
 
 int
-logger(int level, char *msg)
+log_level_string_get(wsfs_str_t* out, int level)
 {
+  switch (level)
+  {
+    case LOGL_EMERG:
+      out->len = strlen(LOGL_STRING_EMERG);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_EMERG;
+      return 0;
+    case LOGL_ALERT:
+      out->len = strlen(LOGL_STRING_ALERT);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_ALERT;
+      return 0;
+    case LOGL_CRIT:
+      out->len = strlen(LOGL_STRING_CRIT);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_CRIT;
+      return 0;
+    case LOGL_ERROR:
+      out->len = strlen(LOGL_STRING_ERROR);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_ERROR;
+      return 0;
+    case LOGL_WARN:
+      out->len = strlen(LOGL_STRING_WARN);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_WARN;
+      return 0;
+    case LOGL_NOTICE:
+      out->len = strlen(LOGL_STRING_NOTICE);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_NOTICE;
+      return 0;
+    case LOGL_INFO:
+      out->len = strlen(LOGL_STRING_INFO);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_INFO;
+      return 0;
+    case LOGL_DEBUG:
+      out->len = strlen(LOGL_STRING_DEBUG);
+      out->string = (char*)malloc(out->len + 1);
+      out->string = LOGL_STRING_DEBUG;
+      return 0;
+    default:
+      return -1;
+  }
+}
+
+int
+logger(int level, wsfs_str_t* source, char *msg)
+{
+  time_t rawtime;
+  struct tm * timeinfo;
+  wsfs_str_t level_string;
+  wsfs_str_t source_string = WSFS_STR_T_CHAR("INTERNAL");
+
   if (level > log_level)
     return -1;
 
-  fprintf(stderr, "%s", msg);
+  if (source != NULL)
+    memcpy(&source_string, source, sizeof(wsfs_str_t));
+
+  log_level_string_get(&level_string, level);
+
+  time( &rawtime );
+  timeinfo = localtime ( &rawtime );
+  char timestamp_string[32];
+  strftime(timestamp_string, 32, "%F %T %z", timeinfo);
+
+  fprintf(stderr, "%s - [%s] - %s - \"%s\"\n", source_string.string, timestamp_string, level_string.string, msg);
   return 0;
 }
-
--- src/logger.h
+++ src/logger.h
@@ -5,9 +5,11 @@
 
 #include <linux/limits.h>
 
+#include "wsfs_core.h"
+
 extern int log_level;
 extern char log_output[PATH_MAX];
 
-int logger(int level, char *msg);
+int logger(int level, wsfs_str_t* source, char *msg);
 
 #endif
